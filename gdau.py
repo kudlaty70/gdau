e69908d225537b9a818c9a5559b5e7ce268ae8ad
Tomasz Mroz <tomasz.mroz@nmbu.no>
2021-07-14 11:42:58 +0200

    Mostly aesthetic changes not affecting how the script works. Corrected a couple of typo's and changed some variable names to look better and be more descriptive.

--- gdau.py
+++ gdau.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Version with command line arguments
 import os
 import time
 import argparse
 import glob
 import shutil
 from pathlib import Path
-#from tkinter import filedialog
-#from tkinter import *
 import datetime
 import urllib.request
 def connect(host='http://google.com'):
@@ -35,10 +32,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--date", type=str, required = True)
 parser.add_argument("--drone", type=str, required = True)
 parser.add_argument("--path", type=str, required = True)
+parser.add_argument("--destinationpath", type=str, required=False, default="//largefile.nmbu.no/Project/V-Pheno/Raw_images/2021")
 parser.add_argument("--counterres", type = int, default = 5, required = False)
 args = parser.parse_args()
-counter_resolution = 1
-#date = input('Date of flight? (yyyy-mm-dd)\n')
+counter_resolution = args.counterres
 date = args.date
 year, month, day = date.split('-')
 isValidDate = True
@@ -48,20 +45,16 @@ except ValueError:
     isValidDate = False
 if not isValidDate:
     quit()
-#drone = input('Which drone? P4M, P4RGB or Micasense?\n')
 drone = args.drone
-destination = Path("//largefile.nmbu.no/Project/V-Pheno/Raw_images/2021")
+destination = Path(args.destinationpath)
 if drone in ['P4M', 'P4RGB', 'Micasense']:
     destination = os.path.join(destination, drone)
     print('Drone recognized...\n')
 else:
     print('Invalid drone!\n')
     quit()
-
-#path_test = filedialog.askdirectory(title = 'Select image directory')
-path_test = args.path
-directories = glob.glob(os.path.join(path_test, '*'))
-
+path_from = args.path
+directories = glob.glob(os.path.join(path_from, '*'))
 expected_folders = ['graminor_cal', 'graminor_images',
                    'masbasis_cal', 'masbasis_images',
                    'nobalyield_cal', 'nobalyield_images',
@@ -69,21 +62,14 @@ expected_folders = ['graminor_cal', 'graminor_images',
                    'nobalnue_cal', 'nobalnue_images']
 actual_folders = []
 for field in expected_folders:
-    if os.path.join(path_test, field) in directories:
-        actual_folders.append(os.path.join(path_test, field))
-print(str(len(actual_folders)), end = '')
-print(' folders found in the directory...\n')
-temp = []
+    if os.path.join(path_from, field) in directories:
+        actual_folders.append(os.path.join(path_from, field))
+print(f"{str(len(actual_folders))} folders found in the directory...\n")
+fields_present = []
 for field_path in actual_folders:
-    temp.append(os.path.basename(os.path.normpath(field_path)).split('_')[0])
-temp = list(dict.fromkeys(temp))
-# Check if the directory exists already!
-#to_path = os.path.join(destination, field, date)
-for field in temp:
-    #itr = 1
-    #while os.path.isdir(to_path):
-        #to_path = os.path.join(to_path, "_", itr)
-        #itr += 1
+    fields_present.append(os.path.basename(os.path.normpath(field_path)).split('_')[0])
+fields_present = list(dict.fromkeys(fields_present))
+for field in fields_present:
     os.mkdir(os.path.join(destination, field, date))
     if not drone == 'Micasense':
         os.mkdir(os.path.join(destination, field, date, 'RGB'))
@@ -94,48 +80,47 @@ for field in temp:
         os.mkdir(os.path.join(destination, field, date, 'Multispectral', 'images'))
         os.mkdir(os.path.join(destination, field, date, 'Multispectral', 'cal'))
 print('Directories created!\n')
-# Generate raport of upload
-raport = open(os.path.join(destination,'upload_raports',(date + '_' + drone + "_" + "_".join(temp) + "_upload_raport.txt")),"w")
-raport.write("::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n")
-raport.write("::::::::::UPLOAD RAPORT OF IMAGES TO LARGEFILE::::::::::\n")
-raport.write("::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n")
+report = open(os.path.join(destination,'upload_reports',(date + '_' + drone + "_" + "_".join(fields_present) + "_upload_report.txt")),"w")
+report.write("::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n")
+report.write("::::::::::UPLOAD report OF IMAGES TO LARGEFILE::::::::::\n")
+report.write("::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n")
 data_format = "%d/%m/%Y %H:%M:%S"
-raport.write(f"Raport created on: {datetime.datetime.now().strftime(data_format)}\n")
-raport.write("Summary of user input:\n")
-raport.write(f"\tFlight date:\t{date}\n")
-raport.write(f"\tDrone:\t{drone}\n")
-raport.write(f"\tUpload source:\t{path_test}\n")
-raport.write(f"Total directory size:\t{round(get_size(path_test)/1048576, ndigits=2)} Mb\n")
-raport.write(f"Target path:\t{destination}\n")
-raport.write(f"Number of fields provided:\t{str(len(temp))}\n")
-raport.write("Folders provided and number of photos:\n")
-for field in temp:
+report.write(f"report created on: {datetime.datetime.now().strftime(data_format)}\n")
+report.write("Summary of user input:\n")
+report.write(f"\tFlight date:\t{date}\n")
+report.write(f"\tDrone:\t{drone}\n")
+report.write(f"\tUpload source:\t{path_from}\n")
+report.write(f"Total directory size:\t{round(get_size(path_from)/1048576, ndigits=2)} Mb\n")
+report.write(f"Target path:\t{destination}\n")
+report.write(f"Number of fields provided:\t{str(len(fields_present))}\n")
+report.write("Folders provided and number of photos:\n")
+for field in fields_present:
     field_dirs = [dir for dir in actual_folders if field in dir]
-    raport.write(f"\t{field}:\n")
-    raport.write(f"\t\tcal: {str(len(os.listdir(field_dirs[0])))}")
-    raport.write(f"\timages:\t{str(len(os.listdir(field_dirs[1])))}")
-    raport.write(f"\tsum: {str(len(os.listdir(field_dirs[0])) + len(os.listdir(field_dirs[1])))}\n")
+    report.write(f"\t{field}:\n")
+    report.write(f"\t\tcal: {str(len(os.listdir(field_dirs[0])))}")
+    report.write(f"\timages:\t{str(len(os.listdir(field_dirs[1])))}")
+    report.write(f"\tsum: {str(len(os.listdir(field_dirs[0])) + len(os.listdir(field_dirs[1])))}\n")
 total_sum = 0
-for field in temp:
+for field in fields_present:
     field_dirs = [dir for dir in actual_folders if field in dir]
     for subfield in field_dirs:
         total_sum = total_sum + len(os.listdir(subfield))
-raport.write(f"Total number of images:\t{str(total_sum)}\n")
-raport.write("Internet status:\t")
-raport.write("Connected\n" if connect() else "No connection, aborting...\n")
-raport.write(f"LargeFile available:\t{str(os.path.isdir(destination))}\n")
-raport.write(f"Upload started:\t{datetime.datetime.now().strftime(data_format)}\n")
-raport.write('Detailed upload progress:\n')
-print('Raport opened!\n')
+report.write(f"Total number of images:\t{str(total_sum)}\n")
+report.write("Internet status:\t")
+report.write("Connected\n" if connect() else "No connection, aborting...\n")
+report.write(f"LargeFile available:\t{str(os.path.isdir(destination))}\n")
+report.write(f"Upload started:\t{datetime.datetime.now().strftime(data_format)}\n")
+report.write('Detailed upload progress:\n')
+print('report opened!\n')
 print('Starting upload!\n')
 time.sleep(4)
 start_time = datetime.datetime.now()
 # for P4RGB
 if drone == 'P4RGB':
     counter = 0
-    for field in temp:
+    for field in fields_present:
         field_dirs = [dir for dir in actual_folders if field in dir]
-        raport.write(f"{datetime.datetime.now().strftime(data_format)}\tCopying images for field:\t{field}\n")
+        report.write(f"{datetime.datetime.now().strftime(data_format)}\tCopying images for field:\t{field}\n")
         for source_dir in field_dirs:
             files_to_transfer = os.listdir(source_dir)
             if '_cal' in source_dir:
@@ -144,7 +129,7 @@ if drone == 'P4RGB':
                     file = os.path.join(source_dir, file)
                     shutil.copy(file, target_path)
                     counter = counter + 1
-                    raport.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
+                    report.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
                     if counter % counter_resolution == 0:
                         progress_time = (datetime.datetime.now() - start_time)/counter
                         estimated = (total_sum - counter)*progress_time
@@ -158,7 +143,7 @@ if drone == 'P4RGB':
                     file = os.path.join(source_dir, file)
                     shutil.copy(file, target_path)
                     counter = counter + 1
-                    raport.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto: {target_path}\n")
+                    report.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto: {target_path}\n")
                     if counter % counter_resolution == 0:
                         progress_time = (datetime.datetime.now() - start_time)/counter
                         estimated = (total_sum - counter)*progress_time
@@ -169,9 +154,9 @@ if drone == 'P4RGB':
 # for Micasense
 if drone == 'Micasense':
     counter = 0
-    for field in temp:
+    for field in fields_present:
         field_dirs = [dir for dir in actual_folders if field in dir]
-        raport.write(f"{datetime.datetime.now().strftime(data_format)}\tCopying images for field:\t{field}\n")
+        report.write(f"{datetime.datetime.now().strftime(data_format)}\tCopying images for field:\t{field}\n")
         for source_dir in field_dirs:
             files_to_transfer = os.listdir(source_dir)
             if '_cal' in source_dir:
@@ -180,7 +165,7 @@ if drone == 'Micasense':
                     file = os.path.join(source_dir, file)
                     shutil.copy(file, target_path)
                     counter = counter + 1
-                    raport.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
+                    report.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
                     if counter % counter_resolution == 0:
                         progress_time = (datetime.datetime.now() - start_time)/counter
                         estimated = (total_sum - counter)*progress_time
@@ -194,7 +179,7 @@ if drone == 'Micasense':
                     file = os.path.join(source_dir, file)
                     shutil.copy(file, target_path)
                     counter = counter + 1
-                    raport.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
+                    report.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
                     if counter % counter_resolution == 0:
                         progress_time = (datetime.datetime.now() - start_time)/counter
                         estimated = (total_sum - counter)*progress_time
@@ -204,9 +189,9 @@ if drone == 'Micasense':
                         statbar(counter, total_sum)
 if drone == 'P4M':
     counter = 0
-    for field in temp:
+    for field in fields_present:
         field_dirs = [dir for dir in actual_folders if field in dir]
-        raport.write(f"{datetime.datetime.now().strftime(data_format)}\tCopying images for field:\t{field}\n")
+        report.write(f"{datetime.datetime.now().strftime(data_format)}\tCopying images for field:\t{field}\n")
         for source_dir in field_dirs:
             files_to_transfer = os.listdir(source_dir)
             if '_cal' in source_dir:
@@ -216,7 +201,7 @@ if drone == 'P4M':
                         file = os.path.join(source_dir, file)
                         shutil.copy(file, target_path)
                         counter = counter + 1
-                        raport.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
+                        report.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
                         if counter % counter_resolution == 0:
                             progress_time = (datetime.datetime.now() - start_time)/counter
                             estimated = (total_sum - counter)*progress_time
@@ -229,7 +214,7 @@ if drone == 'P4M':
                         file = os.path.join(source_dir, file)
                         shutil.copy(file, target_path)
                         counter = counter + 1
-                        raport.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
+                        report.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
                         if counter % counter_resolution == 0:
                             progress_time = (datetime.datetime.now() - start_time)/counter
                             estimated = (total_sum - counter)*progress_time
@@ -244,7 +229,7 @@ if drone == 'P4M':
                         file = os.path.join(source_dir, file)
                         shutil.copy(file, target_path)
                         counter = counter + 1
-                        raport.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
+                        report.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
                         if counter % counter_resolution == 0:
                             progress_time = (datetime.datetime.now() - start_time)/counter
                             estimated = (total_sum - counter)*progress_time
@@ -257,7 +242,7 @@ if drone == 'P4M':
                         file = os.path.join(source_dir, file)
                         shutil.copy(file, target_path)
                         counter = counter + 1
-                        raport.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
+                        report.write(f"{datetime.datetime.now().strftime(data_format)}\tFile: {file}\tto:\t{target_path}\n")
                         if counter % counter_resolution == 0:
                             progress_time = (datetime.datetime.now() - start_time)/counter
                             estimated = (total_sum - counter)*progress_time
@@ -267,8 +252,8 @@ if drone == 'P4M':
                             statbar(counter, total_sum)
 end_time = datetime.datetime.now()
 duration = end_time - start_time
-raport.write(f"Upload finished at: {datetime.datetime.now().strftime(data_format)}\nTotal images uploaded: {str(counter)}\nTime elapsed: {str(duration)}")
-raport.close()
+report.write(f"Upload finished at: {datetime.datetime.now().strftime(data_format)}\nTotal images uploaded: {str(counter)}\nTime elapsed: {str(duration)}")
+report.close()
 
 
 
